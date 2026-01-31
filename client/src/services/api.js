f7b484b01272c9a45a368754859d18d3cbf4dde1
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-01-31 12:19:53 +0530

    Fix API baseURL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -21,7 +21,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3002/api"
-  baseURL: "api",
+  baseURL: "https://finsmart-backend-dji4.onrender.com/api",
   withCredentials: true,
 });
 
