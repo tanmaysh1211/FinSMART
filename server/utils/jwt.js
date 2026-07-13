461db658d476ed0ddda386ce53835beff3058b63
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-07-13 17:31:16 +0530

    Update jwt.js

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,8 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// export const signToken = (user) =>
-//   jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-
 import jwt from "jsonwebtoken";
 
 export const signToken = (user) => {
