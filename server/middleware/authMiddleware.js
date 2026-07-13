fc27220538e36ac252e1d3684e958e3015bbe0ed
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-07-13 17:38:05 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,13 +2,10 @@ import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
   let token;
-
-  //  Header token (normal APIs)
   if (req.headers.authorization?.startsWith("Bearer ")) {
     token = req.headers.authorization.split(" ")[1];
   }
 
-  //  Query token (SSE)
   if (!token && req.query.token) {
     token = req.query.token;
   }
@@ -17,17 +14,8 @@ const authMiddleware = (req, res, next) => {
     return res.status(401).json({ message: "No token provided" });
   }
 
-  // if (req.path.includes("chat-stream")) {
-  // req.user = { id: decoded.id };
-  // return next();
-// }
-
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // req.user = decoded;
-    // next();
-
     req.user = { id: decoded.id };
     return next();
 
