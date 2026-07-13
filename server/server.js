432f6b0b48846a43f46f352e77da3b99138a1533
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-07-13 17:29:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 import "./loadEnv.js";
 import express from "express";
 import cors from "cors";
-// import path from "path";
-// import { fileURLToPath } from "url";
-
 import helmet from "helmet";
 import passport from "passport";
-
 import connectDB from "./config/db.js";
 import "./config/passport.js";
-
 import authRoutes from "./routes/auth.routes.js";
 import transactionRoutes from "./routes/transaction.routes.js";
 import analyticsRoutes from "./routes/analytics.routes.js";
@@ -22,7 +17,6 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-// app.use(cors());
 app.use(
   cors({
     origin:process.env.CLIENT_URL,
@@ -34,13 +28,10 @@ app.use(
 app.use(helmet());
 app.use(express.json());
 app.use(passport.initialize());
-
 app.use("/api/auth", authRoutes);
-// const transactionRoutes = require("./routes/transaction.routes.js");
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.use("/api/chatbot", chatbotRoutes);  
-
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3002;
