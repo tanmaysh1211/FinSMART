f880462622d6c8d335b66be6752046b4723a1e23
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-07-13 17:08:18 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
-// import { AuthProvider } from "./context/AuthContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     {/* <AuthProvider> */}
-//       <App />
-//     {/* </AuthProvider> */}
-//   </React.StrictMode>
-// );
-
-
-
 import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
