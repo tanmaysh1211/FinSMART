c784a8c7d91c8d6b11b75e584bbb38f6a5b80a1e
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-07-13 17:26:49 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,14 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  // const { user } = useAuth();
-  // if (!user) return <Navigate to="/login" />;
   const { user, loading } = useAuth();
-
   if (loading) return null; 
-
   if (!user) return <Navigate to="/login" />;
-
   return children;
 };
 
