455d69ce2d89139eff8428bf51b17e6ecdc28cda
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2026-07-13 17:32:31 +0530

    Update chatbot.routes.js

--- server/routes/chatbot.routes.js
+++ server/routes/chatbot.routes.js
@@ -1,24 +1,8 @@
-// import express from "express";
-// import { chatMessage } from "../controllers/chatbot.controller.js";
-// import { protect } from "../middleware/authMiddleware.js";
-
-// const router = express.Router();
-
-// // POST /api/chatbot/message
-// router.post("/message", protect, chatMessage);
-
-// export default router;
-
-
-
-
 import express from "express";
 import { chatMessage } from "../controllers/chatbot.controller.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
-// POST /api/chatbot/message
 router.post("/message", authMiddleware, chatMessage);
 
 export default router;
